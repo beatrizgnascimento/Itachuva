221612eebb12538ed98462dd0219fdd983c6f3d4
Toti <gabrieltoti414@gmail.com>
2026-06-02 17:31:51 -0300

    feat(get-open-files-service): corrige a verificação de download.

--- pipeline/get_open_files_service/src/file_download_helpers.py
+++ pipeline/get_open_files_service/src/file_download_helpers.py
@@ -2,5 +2,6 @@ import os
 
 from shared.utils.grib_processing import get_grib_folder
 
-def check_file_download():
-    return os.path.exists(get_grib_folder())
+def check_file_download(step_count: int):
+
+    return os.path.exists(get_grib_folder()) and len([f for f in os.listdir(get_grib_folder()) if os.path.isfile(f)]) == step_count
--- pipeline/get_open_files_service/src/main.py
+++ pipeline/get_open_files_service/src/main.py
@@ -2,7 +2,6 @@ from ecmwf_open import get_open_files
 from file_download_helpers import check_file_download
 from shared.config.env import init_env, get_env
 import time
-import os
 
 
 from shared.utils.datecycle import get_cycle, get_date
@@ -12,13 +11,15 @@ init_env()
 download_date = get_date()
 download_cycle = get_cycle()
 
-if check_file_download():
+steps = list(range(0, 27, 3))
+
+if check_file_download(len(steps)):
     print("Arquivos já baixados. Ignorando download.")
     exit(0)
 
 st = time.time()
-
-get_open_files(download_date, cycle=download_cycle, params=get_env().get("VARIABLES"), steps=list(range(0, 27, 3)))
+print(get_env().get("VARIABLES"))
+get_open_files(download_date, cycle=download_cycle, params=get_env().get("VARIABLES"), steps=steps)
 
 ed = time.time()
 
