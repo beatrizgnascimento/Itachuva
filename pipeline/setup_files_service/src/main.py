9387db6b0b05a7bf05d380c3995f09e2d24cbd11
Toti <gabrieltoti414@gmail.com>
2026-06-03 14:53:18 -0300

    feat(setup-files-service): Adiciona limpeza de arquivos .grib intermediários.

--- pipeline/setup_files_service/src/main.py
+++ pipeline/setup_files_service/src/main.py
@@ -1,3 +1,4 @@
+import shutil
 from clip_variables import TARGET_VARS, processing_grib
 from shared.config.env import get_env, init_env
 from shared.utils.validate_files import verify_tiffs
@@ -6,3 +7,4 @@ init_env()
 
 processing_grib() 
 verify_tiffs(TARGET_VARS, get_env()['GTIFF_OUT'])
+shutil.rmtree(get_env()['GRIB_OUT'])
